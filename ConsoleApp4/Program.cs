e6051a942b96fb337fc8dcdb343567598bbde7ff
student <student@S-24-01>
2023-03-11 19:01:52 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b, c;
+a = 1;
+b = 2;
+c = 3;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//var bb = true;
+//var fb = false;
+//if (!bb == true)
+//else if (fb == true && (bb == true && fb == false))
+//    {
+//        Console.WriteLine("Empty");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Any");
+//    }
+
+var test = 12;
+var me = 10;
+switch (test)
+{
+    case 1: Console.WriteLine(1);
+            break;
+        case 10:
+        Console.WriteLine(true);
+            break;
+    default:
+        Console.WriteLine(test);
+        break;
+}
